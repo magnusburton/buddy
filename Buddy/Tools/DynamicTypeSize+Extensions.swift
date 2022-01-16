4c95fad87e16dce1bd73aa04c2a7251ff249e8b2
Magnus Burton <magnus.burton@me.com>
2022-01-16 11:22:23 +0100

    Dynamic type size extensions

---
+++ Buddy/Tools/DynamicTypeSize+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  DynamicTypeSize+Extensions.swift
+//  Buddy
+//
+//  Created by Magnus Burton on 2021-11-11.
+//
+
+import SwiftUI
+
+extension DynamicTypeSize {
+	static let `default` = Self.large
+}
